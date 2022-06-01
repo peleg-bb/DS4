74fe0f83ed4bd3bb33f6a2cc1160fbf227312f09
Peleg <benbapel@post.bgu.ac.il>
2022-06-01 16:20:34 +0300

    adding VPL files

---
+++ BacktrackingBTree.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+public class BacktrackingBTree<T extends Comparable<T>> extends BTree<T> {
+	// For clarity only, this is the default ctor created implicitly.
+	public BacktrackingBTree() {
+		super();
+	}
+
+	public BacktrackingBTree(int order) {
+		super(order);
+	}
+
+	//You are to implement the function Backtrack.
+	public void Backtrack() {
+		// You should remove the next two lines, after double-checking that the signature is valid!
+	    IntegrityStatement.signature(); // Reminder!
+	    throw new UnsupportedOperationException("You should implement this");
+    }
+	
+	//Change the list returned to a list of integers answering the requirements
+	public static List<Integer> BTreeBacktrackingCounterExample(){
+		// You should remove the next two lines, after double-checking that the signature is valid!
+	    IntegrityStatement.signature(); // Reminder!
+	    throw new UnsupportedOperationException("You should implement this");
+	}
+}
